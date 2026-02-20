3a9f0bc8982b1d13a84e2796ba0bc715d3d5c0b5
moondiverc <nezzaluna10@gmail.com>
2026-02-21 01:33:56 +0700

    feat: stock feature state management

--- lib/features/stock/presentation/cubit/stock_cubit.dart
+++ lib/features/stock/presentation/cubit/stock_cubit.dart
@@ -1,8 +1,24 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:stock_app/core/usecase/usecase.dart';
+import 'package:stock_app/features/stock/domain/entities/stock.dart';
+import 'package:stock_app/features/stock/domain/usecases/get_all_stock.dart';
 
 part 'stock_state.dart';
 
 class StockCubit extends Cubit<StockState> {
-  StockCubit() : super(StockInitial());
+  final GetAllStock _getStocksUseCase;
+
+  StockCubit(this._getStocksUseCase) : super(StockInitial());
+
+  Future<void> getStocks() async {
+    emit(StockLoading());
+
+    final result = await _getStocksUseCase(NoParams());
+
+    result.fold(
+      (failure) => emit(StockFailure(failure.message)),
+      (stocks) => emit(StockLoaded(stocks)),
+    );
+  }
 }
--- lib/features/stock/presentation/cubit/stock_state.dart
+++ lib/features/stock/presentation/cubit/stock_state.dart
@@ -4,3 +4,17 @@ part of 'stock_cubit.dart';
 sealed class StockState {}
 
 final class StockInitial extends StockState {}
+
+final class StockLoading extends StockState {}
+
+final class StockLoaded extends StockState {
+  final List<Stock> stocks;
+
+  StockLoaded(this.stocks);
+}
+
+final class StockFailure extends StockState {
+  final String error;
+
+  StockFailure(this.error);
+}
