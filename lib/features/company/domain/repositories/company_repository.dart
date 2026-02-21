e8837480651cc21520120366166abd442d505b20
moondiverc <nezzaluna10@gmail.com>
2026-02-22 03:11:58 +0700

    feat: company repository interface

---
+++ lib/features/company/domain/repositories/company_repository.dart
@@ -0,0 +1,7 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:stock_app/core/error/failures.dart';
+import 'package:stock_app/features/company/domain/entities/company.dart';
+
+abstract interface class CompanyRepository {
+  Future<Either<Failure, Company>> getCompany(String ticker);
+}
